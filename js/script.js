1773da99bcf3c7b119c0c0af55587a26d344c509
Tina Araujo <itaraujo8720@gmail.com>
2018-11-13 08:34:39 -0600

    Combined the printQuote and getrandomColor functions

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ const spanYear = document.querySelector('.year');
 const loadQuote = document.querySelector('#loadQuote');
 const body = document.querySelector('.body');
 
+//let interval = window.setInterval(
 // Create the array of quote objects and name it quotes
+
 var quotes = [
     {
         quote: 'I am not afraid of storms for I am learning how to sail my ship.',
@@ -35,6 +37,13 @@ var quotes = [
         source: 'Aristotle Onassis',
     }
   ]
+
+
+
+  loadQuote.addEventListener("click", function (event){
+    printQuote();
+    getRandomColor();
+  });
 // Create the getRandomQuuote function and name it getRandomQuote
 //***return a randomly selected quote
 
@@ -44,6 +53,9 @@ function getRandomQuote(array) {
     return rand_quote;
   };
 // Create the printQuote funtion and name it printQuote
+
+
+
 function printQuote () {
 
 //I called the getRandomQuote function first then put it in a variable
@@ -62,33 +74,59 @@ function printQuote () {
         + '<p class="source">' +  rand_Q2.source + '</p>';
         quotebox.innerHTML = message;
       }
+//Random Color Generator - Placed the function inside the printOuote function
+      function getRandomColor () {
+          let rand_color = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+          let newArrCol = [];
+                  for (let i = 0; i < 6; i++) {
+                  let rand_col = Math.floor(Math.random() * rand_color.length);
+                  let newNum = rand_color[rand_col];
+                  newArrCol.push(newNum);
+                  };
+                return newArrCol;
+        };
+        let new_Arr = getRandomColor();
+        let message1 = '#' + new_Arr[0] + new_Arr[1] + new_Arr[2] + new_Arr[3] + new_Arr[4] + new_Arr[5];
+        body.style.backgroundColor = message1;
+        loadQuote.style.backgroundColor = message1;
+
   };
 //Calling the function to be printed to the page
 printQuote();
+let setInterval = document.setInterval(printOuote, 20*1000);
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-//Random Color Generator
-loadQuote.addEventListener("click", function (event) {
-  //I placed the random color function inside the event handler
-
-  let rand_color = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
-
-  function getRandomColor (){
-
-    let newArrCol = [];
-            for (let i = 0; i < 6; i++) {
-            let rand_col = Math.floor(Math.random() * rand_color.length);
-            let newNum = rand_color[rand_col];
-            newArrCol.push(newNum);
-          };
-  return newArrCol;
-  };
-  let new_Arr = getRandomColor();
-  let message = '#' + new_Arr[0] + new_Arr[1] + new_Arr[2] + new_Arr[3] + new_Arr[4] + new_Arr[5];
-  console.log(message);
-  body.style.backgroundColor = message;
-  loadQuote.style.backgroundColor = message;
-});
 
 
-loadQuote.addEventListener("click", printQuote, false);
+//loadQuote.addEventListener("click", printQuote, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//****
